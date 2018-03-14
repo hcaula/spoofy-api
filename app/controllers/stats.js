ee16626decdbe3866608e04785d5aa313824f8c0
hcaula <henrique.caula@hotmail.com>
2018-03-14 01:22:12 -0300

    Returns sorted genres per user

--- app/controllers/stats.js
+++ app/controllers/stats.js
@@ -4,6 +4,7 @@ const User = require('mongoose').model('User');
 const Track = require('mongoose').model('Track');
 const User_Track = require('mongoose').model('User_Track');
 
+const util = require('../lib/util');
 const authenticateUser = require('../lib/auth').authenticateUser;
 const errors = require('../lib/errors').errors;
 
@@ -19,14 +20,6 @@ let getUsersMostListenedGenres = function(req, res) {
             res.status(500).json(errors[500]);
         } else if(uTracks.length == 0) res.status(200).json({genres: []});
         else {
-            uTracks.sort((a, b) => {
-                let lastA = a.played_at[a.played_at.length-1];
-                let lastB = b.played_at[b.played_at.length-1];
-                if(lastA < lastB) return 1;
-                else if (lastB < lastA) return -1;
-                else return 0;
-            });
-
             let trackIds = uTracks.map(u => u.track);
             Track.find({_id: {$in: trackIds}}, function(error, tracks){
                 if(error) {
@@ -39,7 +32,27 @@ let getUsersMostListenedGenres = function(req, res) {
                             genres.push(g);
                         })
                     });
-                    res.status(200).json({genres: genres});
+
+                    let countedGenres = [];
+                    let count = 0;
+                    for(let i = 0; i < genres.length; i++) {
+                        if(util.searchByField("genre", genres[i], countedGenres) == -1){
+                            let a = genres[i];
+                            for(let j = 0; j < genres.length; j++) {
+                                let b = genres[j];
+                                if(a == b) count++;
+                            }
+                            countedGenres.push({
+                                genre: a,
+                                times_listened: count
+                            });
+                            count = 0;
+                        }
+                    }
+
+                    countedGenres.sort((a, b) => b.times_listened - a.times_listened);
+
+                    res.status(200).json({genres: countedGenres});
                 }
             });
         }
