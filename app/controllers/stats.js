041a7b8d33f9eebbee79c845c5563a839ae30ee1
hcaula <henrique.caula@hotmail.com>
2018-03-14 03:25:35 -0300

    Creates a get genre by track ids for modularization

--- app/controllers/stats.js
+++ app/controllers/stats.js
@@ -9,14 +9,12 @@ const authenticateUser = require('../lib/auth').authenticateUser;
 const errors = require('../lib/errors').errors;
 
 module.exports = function(app) {
-    app.get('/v1/stats/genre/user', authenticateUser, getUsersListenedGenres);
-    app.get('/v1/stats/genre/user/hour', authenticateUser, getUsersListenedGenresByHour);
+    app.get('/v1/stats/genre/user', authenticateUser, getUserTracks, getGenresByTracks);
+    app.get('/v1/stats/genre/user/hour', authenticateUser, getUserTracksByHour, getGenresByTracks);
 }
 
-let getUsersListenedGenres = function(req, res) {
+let getUserTracks = function(req, res, next) {
     let user = req.user;
-    let page = (req.query.page || 0);
-    let limit = 20;
 
     User_Track.find({user: user}, function(error, uTracks){
         if(error) {
@@ -24,53 +22,16 @@ let getUsersListenedGenres = function(req, res) {
             res.status(500).json(errors[500]);
         } else if(uTracks.length == 0) res.status(200).json({genres: []});
         else {
-            let trackIds = uTracks.map(u => u.track);
-            Track.find({_id: {$in: trackIds}}, function(error, tracks){
-                if(error) {
-                    winston.error(error);
-                    res.status(500).json(errors[500]);   
-                } else {
-                    let genres = []
-                    tracks.forEach(t => {
-                        t.genres.forEach(g => {
-                            genres.push(g);
-                        })
-                    });
-
-                    let countedGenres = [];
-                    let count = 0;
-                    for(let i = 0; i < genres.length; i++) {
-                        if(util.searchByField("genre", genres[i], countedGenres) == -1){
-                            let a = genres[i];
-                            for(let j = 0; j < genres.length; j++) {
-                                let b = genres[j];
-                                if(a == b) count++;
-                            }
-                            countedGenres.push({
-                                genre: a,
-                                times_listened: count
-                            });
-                            count = 0;
-                        }
-                    }
-
-                    countedGenres.sort((a, b) => b.times_listened - a.times_listened);
-                    let total = countedGenres.length;
-                    genres = countedGenres.splice(page*limit, limit);
-                    
-                    res.status(200).json({genres: genres, total: total});
-                }
-            });
+            req.track_ids = uTracks.map(u => u.track);
+            next();
         }
     });
 }
 
-let getUsersListenedGenresByHour = function(req, res) {
+let getUserTracksByHour = function(req, res, next) {
     let user = req.user;
-    let page = (req.query.page || 0);
     let begin = req.query.begin;
     let end = (req.query.end || 0);
-    let limit = 20;
 
     if(!begin) res.status(400).json(errors[400]('begin'));
     else if (parseInt(begin) >= parseInt(end)) {
@@ -101,46 +62,57 @@ let getUsersListenedGenresByHour = function(req, res) {
                     }
                 });
 
-                let trackIds = uTracks.map(u => u.track);
-                Track.find({_id: {$in: trackIds}}, function(error, tracks){
-                    if(error) {
-                        winston.error(error);
-                        res.status(500).json(errors[500]);   
-                    } else {
-                        let genres = []
-                        tracks.forEach(t => {
-                            t.genres.forEach(g => {
-                                genres.push(g);
-                            })
-                        });
-
-                        let countedGenres = [];
-                        let count = 0;
-                        for(let i = 0; i < genres.length; i++) {
-                            if(util.searchByField("genre", genres[i], countedGenres) == -1){
-                                let a = genres[i];
-                                for(let j = 0; j < genres.length; j++) {
-                                    let b = genres[j];
-                                    if(a == b) count++;
-                                }
-                                countedGenres.push({
-                                    genre: a,
-                                    times_listened: count
-                                });
-                                count = 0;
-                            }
-                        }
-
-                        countedGenres.sort((a, b) => b.times_listened - a.times_listened);
-                        let total = countedGenres.length;
-                        genres = countedGenres.splice(page*limit, limit);
-
-                        res.status(200).json({genres: genres, total: total});
-                    }
-                });
+                req.track_ids = uTracks.map(u => u.track);
+                next();
             }
         });
 
     }
 
+}
+
+let getGenresByTracks = function(req, res) {
+    let page = (req.query.page || 0);
+    let limit = 20;
+
+    let track_ids = (req.body.track_ids || req.track_ids);
+    if(!track_ids) res.status(400).json(errors[400]('track_ids'));
+    else {
+        Track.find({_id: {$in: track_ids}}, function(error, tracks){
+            if(error) {
+                winston.error(error);
+                res.status(500).json(errors[500]);   
+            } else {
+                let genres = []
+                tracks.forEach(t => {
+                    t.genres.forEach(g => {
+                        genres.push(g);
+                    })
+                });
+    
+                let countedGenres = [];
+                let count = 0;
+                for(let i = 0; i < genres.length; i++) {
+                    if(util.searchByField("genre", genres[i], countedGenres) == -1){
+                        let a = genres[i];
+                        for(let j = 0; j < genres.length; j++) {
+                            let b = genres[j];
+                            if(a == b) count++;
+                        }
+                        countedGenres.push({
+                            genre: a,
+                            times_listened: count
+                        });
+                        count = 0;
+                    }
+                }
+    
+                countedGenres.sort((a, b) => b.times_listened - a.times_listened);
+                let total = countedGenres.length;
+                genres = countedGenres.splice(page*limit, limit);
+    
+                res.status(200).json({genres: genres, total: total});
+            }
+        });
+    }
 }
