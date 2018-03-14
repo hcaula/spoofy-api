651da3083ea20f5a684e4dd093dc14f0e84ba6c3
hcaula <henrique.caula@hotmail.com>
2018-03-14 00:48:36 -0300

    Sorts tracks by last listened at

--- app/controllers/stats.js
+++ app/controllers/stats.js
@@ -1,19 +1,37 @@
-const authenticateUser = require('../lib/auth').authenticateUser;
+const winston = require('winston');
 
 const User = require('mongoose').model('User');
+const Track = require('mongoose').model('Track');
+const User_Track = require('mongoose').model('User_Track');
+
+const authenticateUser = require('../lib/auth').authenticateUser;
+const errors = require('../lib/errors').errors;
 
 module.exports = function(app) {
-    app.get('/v1/stats/', authenticateUser, getUser);
+    app.get('/v1/stats/genre/user', authenticateUser, getUsersMostListenedGenres);
 }
 
-let getUser = function(req, res) {
+let getUsersMostListenedGenres = function(req, res) {
     let user = req.user;
-    res.status(200).json({
-        _id: user._id,
-        display_name: user.display_name,
-        email: user.email,
-        uri: user.uri,
-        href: user.href,
-        images: user.images
+    User_Track.find({user: user}, function(error, uTracks){
+        if(error) {
+            winston.error(error);
+            res.status(500).json(errors[500]);
+        } else if(uTracks.length == 0) res.status(200).json({genres: []});
+        else {
+            uTracks.sort((a, b) => {
+                let lastA = a.played_at[a.played_at.length-1];
+                let lastB = b.played_at[b.played_at.length-1];
+                if(lastA < lastB) return 1;
+                else if (lastB < lastA) return -1;
+                else return 0;
+            });
+            res.status(200).json({tracks: uTracks});
+
+            // let trackIds = uTracks.map(u => u.track);
+            // Track.find({_id: {$in: trackIds}}, function(error){
+
+            // });
+        }
     });
 }
