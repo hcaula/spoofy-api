7a48c0bbad33b496126e4122772bc9e5a3d81195
hcaula <henrique.caula@hotmail.com>
2018-03-14 03:53:51 -0300

    Makes genre query only one route

--- app/controllers/stats.js
+++ app/controllers/stats.js
@@ -9,41 +9,41 @@ const authenticateUser = require('../lib/auth').authenticateUser;
 const errors = require('../lib/errors').errors;
 
 module.exports = function(app) {
-    app.get('/v1/stats/genre/user', authenticateUser, getUserTracks, getGenresByTracks);
-    app.get('/v1/stats/genre/user/hour', authenticateUser, getUserTracksByHour, getGenresByTracks);
+    app.get('/v1/stats/genre/user', authenticateUser, getUserTracksByHour, getGenresByTracks);
 }
 
-let getUserTracks = function(req, res, next) {
-    let user = req.user;
-
-    User_Track.find({user: user}, function(error, uTracks){
-        if(error) {
-            winston.error(error);
-            res.status(500).json(errors[500]);
-        } else if(uTracks.length == 0) res.status(200).json({genres: []});
-        else {
-            req.track_ids = uTracks.map(u => u.track);
-            next();
-        }
-    });
-}
-
+/* Gets ser tracks by hour */
+/* If a begin time comes with the request, gets only the user-tracks for this hour */
+/* If an end time comes with the request, gets the user-tracks for the period begin-end */
+/* If neither begin or end time come with the request, gets all user-tracks */
 let getUserTracksByHour = function(req, res, next) {
     let user = req.user;
-    let begin = req.query.begin;
-    let end = (req.query.end || 0);
+    let begin = (parseInt(req.query.begin) || 0);
+    
+    let end;
+    if(!req.query.begin && !req.query.end) end = 24;
+    else if (req.query.begin && !req.query.end) end = begin;
+    else if (req.query.begin && req.query.end) end = parseInt(req.query.end)
+    else {
+        res.status(400).json({
+            type: 'bad_request',
+            error: "An end time should come with a begin time."
+        });
+    }
 
-    if(!begin) res.status(400).json(errors[400]('begin'));
-    else if (parseInt(begin) >= parseInt(end)) {
+    if(begin < 0 || begin > 24 || end < 0 || end > 24) {
+        res.status(400).json({
+            type: 'bad_request',
+            error: 'Invalid hour.'
+        });
+    } else if (begin > end) {
         res.status(400).json({
             type: 'bad_request',
             error: 'The end time should be bigger than the begin time.'
         });
     } else {
-        begin = parseInt(begin), end = parseInt(end);
-
         let hours = [];
-        if(!end) hours = [begin];
+        if(end == begin) hours = [begin];
         else for(let i = begin; i <= (end - begin); i++) hours.push(i);
 
         User_Track.find({user: user}, function(error, _uTracks){
