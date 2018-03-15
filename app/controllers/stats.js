f7877b13186886d8be5162f3b6209fec0eaa1bdf
hcaula <henrique.caula@hotmail.com>
2018-03-15 05:27:10 -0300

    Some refactoring

--- app/controllers/stats.js
+++ app/controllers/stats.js
@@ -1,4 +1,5 @@
 const winston = require('winston');
+const async = require('async');
 
 const User = require('mongoose').model('User');
 const Track = require('mongoose').model('Track');
@@ -9,86 +10,49 @@ const authenticateUser = require('../lib/auth').authenticateUser;
 const errors = require('../lib/errors').errors;
 
 module.exports = function(app) {
-    app.get('/v1/stats/genre/user', authenticateUser, getUserTracksByHourDay, getGenresByTracks);
+    app.get('/v1/stats/genre/user', authenticateUser, getUserTracks, getUserTracksByHourDay, getGenresByTracks, countAndPaginateGenres);
     app.get('/v1/stats/genre/user/time', authenticateUser, getUserTracksDividedByTime);
 }
 
-/* Gets ser tracks by hour */
-/* If a begin time comes with the request, gets only the user-tracks for this time */
-/* If an end time comes with the request, gets the user-tracks for the period begin-end */
-/* If neither begin or end time come with the request, assumes begin = 0 and end = <last> */
-let getUserTracksByHourDay = function(req, res, next) {
-    let user = req.user;
-    
+/*
+ * MIDDLEWARE FUNCTIONS
+*/
 
-    User_Track.find({user: user._id}, function(error, _uTracks){
+/* Gets user-tracks for the token passed user */
+let getUserTracks = function(req, res, next) {
+    let user = req.user;
+
+    User_Track.find({user: user._id}, function(error, user_tracks){
         if(error) {
             winston.error(error.stack);
             res.status(500).json(errors[500]);
         } else {
-            let period = util.filterPerPeriod({
-                begin_hour: req.query.begin_hour,
-                begin_day: req.query.begin_day,
-                end_hour: req.query.end_hour,
-                end_day: req.query.end_day
-            }, "played_at", _uTracks);
-
-            if(period.error) res.status(400).json(period.error);
-            else {
-                req.track_ids = period.array.map(u => u.track);
-                next();
-            }
+            req.user_tracks = user_tracks;
+            next();
         }
     });
-
 }
 
-let getGenresByTracks = function(req, res) {
-    let page = (req.query.page || 0);
-    let limit = 20;
+/* Returns an array of tracks ID's on a pair (begin_hour, end_hour) and/or (begin_day, end_day) */
+let getUserTracksByHourDay = function(req, res, next) {
+    let user_tracks = req.user_tracks;
+    
+    let period = util.filterPerPeriod({
+        begin_hour: req.query.begin_hour,
+        begin_day: req.query.begin_day,
+        end_hour: req.query.end_hour,
+        end_day: req.query.end_day
+    }, "played_at", user_tracks);
 
-    let track_ids = (req.body.track_ids || req.track_ids);
-    if(!track_ids) res.status(400).json(errors[400]('track_ids'));
+    if(period.error) res.status(400).json(period.error);
     else {
-        Track.find({_id: {$in: track_ids}}, function(error, tracks){
-            if(error) {
-                winston.error(error);
-                res.status(500).json(errors[500]);   
-            } else {
-                let genres = []
-                tracks.forEach(t => {
-                    t.genres.forEach(g => {
-                        genres.push(g);
-                    })
-                });
-    
-                let countedGenres = [];
-                let count = 0;
-                for(let i = 0; i < genres.length; i++) {
-                    if(util.searchByField("genre", genres[i], countedGenres) == -1){
-                        let a = genres[i];
-                        for(let j = 0; j < genres.length; j++) {
-                            let b = genres[j];
-                            if(a == b) count++;
-                        }
-                        countedGenres.push({
-                            genre: a,
-                            times_listened: count
-                        });
-                        count = 0;
-                    }
-                }
-    
-                countedGenres.sort((a, b) => b.times_listened - a.times_listened);
-                let total = countedGenres.length;
-                genres = countedGenres.splice(page*limit, limit);
-    
-                res.status(200).json({genres: genres, total: total});
-            }
-        });
+        req.track_ids = period.array.map(u => u.track);
+        next();
     }
 }
 
+/* Gets user-tracks divided by time - ex.: for each day of the week, which tracks were
+played at 4 AM? Or for each hour of the day, which tracks were played on mondays? */
 let getUserTracksDividedByTime = function(req, res, next) {
     let user = req.user;
     let day = req.query.day;
@@ -108,10 +72,72 @@ let getUserTracksDividedByTime = function(req, res, next) {
                 winston.error(error);
                 res.status(500).json(errors[500]);   
             } else {
-                let ret = util.dividePerTime(frequency, choice, "played_at", _uTracks);
-                res.status(200).json(ret);
+                req.user_tracks = util.dividePerTime(frequency, choice, "played_at", _uTracks);
+                next();
             }
         });
     }
+}
 
+/* Given an array of Tracks IDs', returns their genres */
+let getGenresByTracks = function(req, res, next) {
+    let track_ids = (req.body.track_ids || req.track_ids);
+    if(!track_ids) res.status(400).json(errors[400]('track_ids'));
+    else {
+        Track.find({_id: {$in: track_ids}}, function(error, tracks){
+            if(error) {
+                winston.error(error);
+                res.status(500).json(errors[500]);   
+            } else {
+                let genres = []
+                tracks.forEach(t => {
+                    t.genres.forEach(g => {
+                        genres.push(g);
+                    })
+                });
+
+                req.genres = genres;
+                next();
+            }
+        });
+    }
+}
+
+let getGenresDividedByTime = function(req, res, next) {
+}
+
+
+/*
+ * RETURN FUNCTIONS
+*/
+
+/* Count */
+let countAndPaginateGenres = function(req, res, next) {
+    let genres = req.genres;
+    let page = (req.query.page || 0);
+    let limit = 20;
+
+    let countedGenres = [];
+    let count = 0;
+    for(let i = 0; i < genres.length; i++) {
+        if(util.searchByField("genre", genres[i], countedGenres) == -1){
+            let a = genres[i];
+            for(let j = 0; j < genres.length; j++) {
+                let b = genres[j];
+                if(a == b) count++;
+            }
+            countedGenres.push({
+                genre: a,
+                times_listened: count
+            });
+            count = 0;
+        }
+    }
+
+    countedGenres.sort((a, b) => b.times_listened - a.times_listened);
+
+    let total = countedGenres.length;
+    genres = countedGenres.splice(page*limit, limit);
+
+    res.status(200).json({genres: genres, total: total});
 }
