9e1c64ec1ea124e0f6f5a877eee0092406c378bf
hcaula <henrique.caula@hotmail.com>
2018-03-17 23:59:26 -0300

    Adds logout route

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
+const winston = require('winston');
+
+const Session = require('mongoose').model('Session');
 
 const auth_phase = require('../lib/auth_phase');
+const errors = require('../lib/errors');
 
 module.exports = function(app) {
 
@@ -15,7 +19,23 @@ module.exports = function(app) {
 
     app.get('/dashboard', auth_phase, function(req, res){
         if(!req.user) res.redirect('/');
-        else res.status(200).send("Dashboard page");
+        else res.status(200).json({
+            message: "User logged in successfully.",
+            user: req.user.display_name
+        });
     });
 
+    app.get('/logout', auth_phase, function(req, res){
+        if(req.user) {
+            Session.remove({user: req.user._id}, function(error){
+                if(error) {
+                    winston.error(error.stack);
+                    res.status(500).json(errors[500]);
+                } else {
+                    res.clearCookie("spoofy");
+                    res.redirect('/');
+                }
+            });
+        } else res.redirect('/');
+    });
 }
