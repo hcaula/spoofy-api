72d946bb6b832d996a2a4efd1390658af13df47d
hcaula <henrique.caula@hotmail.com>
2018-05-14 03:36:27 -0300

    Writes proper URL on console after server startup

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ async.series([
 ], function (error) {
     if (error) winston.error(error);
     else {
-        winston.info(`Server is ready to receive API calls on ${config.protocol}://${config.hostname}:${config.port}`);
+        winston.info(`Server is ready to receive API calls on ` + 
+        `${config.protocol}://${config.hostname}${config.port ? `:${config.port}` : ''}`);
     }
 });
