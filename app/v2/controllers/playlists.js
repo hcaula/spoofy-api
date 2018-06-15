b26622fa8bca17d2b9fb3fb8e56b203ee886ee1c
hcaula <henrique.caula@hotmail.com>
2018-06-15 10:56:08 -0300

    Adds shared artists and shared tracks route

--- app/v2/controllers/playlists.js
+++ app/v2/controllers/playlists.js
@@ -2,15 +2,17 @@ const winston = require('winston');
 const async = require('async');
 
 const User = require('mongoose').model('User');
+const Artist = require('mongoose').model('Artist');
+const Track = require('mongoose').model('Track');
 
 const errors = require('../lib/errors');
 const auth = require('../lib/auth');
 const { searchByField } = require('../lib/util');
 
-
 module.exports = function (app) {
     app.get('/api/v2/playlists/genres', auth, getUsers, sharedGenres);
     app.get('/api/v2/playlists/artists', auth, getUsers, sharedArtists);
+    app.get('/api/v2/playlists/tracks', auth, getUsers, sharedTracks);
 }
 
 const getUsers = function (req, res, next) {
@@ -20,8 +22,10 @@ const getUsers = function (req, res, next) {
         req.users = [];
         async.eachSeries(users, (id, next) => {
             User.findById(id, (error, user) => {
-                if (error) next(error);
-                else {
+                if (error) {
+                    winston.error(error.stack);
+                    res.status(500).json(errors[500]);
+                } else {
                     req.users.push(user);
                     next();
                 }
@@ -67,19 +71,83 @@ const sharedArtists = function (req, res) {
     const artists = [];
     users.forEach((user, i) => {
         const multiplier = multipliers[i];
-        user.artists.forEach(a => {
-            const index = searchByField(a.name, "name", artists);
-            if (index > -1) artists[index].weight += multiplier;
+        user.artists.forEach((a, j) => {
+            const a_multiplier = user.artists.length - j;
+            const index = searchByField(a, "id", artists);
+            if (index > -1) artists[index].weight += multiplier * a_multiplier;
             else {
                 artists.push({
-                    name: a.name,
-                    weight: multiplier
+                    id: a,
+                    weight: multiplier * a_multiplier
                 });
             }
         });
     });    
+    
+    let new_artists = [];
+    async.each(artists, (a, next) => {
+        Artist.findById(a.id, (error, artist) => {
+            if (error) next(error);
+            else {
+                new_artists.push({
+                    id: a.id,
+                    name: artist.name,
+                    weight: a.weight
+                });
+                next();
+            }
+        });
+    }, error => {
+        if (error) {
+            winston.error(error.stack);
+            res.status(500).json(errors[500]);
+        } else {
+            new_artists = new_artists.sort((a, b) => b.weight - a.weight);        
+            res.status(200).json({ artists: new_artists });
+        }
+    });
+}
 
-    artists.sort((a, b) => b.weight - a.weight);
+const sharedTracks = function (req, res) {
+    const users = req.users;
+    const multipliers = (req.query.multipliers ? req.query.multipliers.split(',') : [].fill.call({ length: users.length }, 1));
 
-    res.status(200).json({ artists: artists });
+    const tracks = [];
+    users.forEach((user, i) => {
+        const multiplier = multipliers[i];
+        user.tracks.forEach((t, j) => {
+            const t_multiplier = user.tracks.length - j;
+            const index = searchByField(t, "id", tracks);
+            if (index > -1) tracks[index].weight += multiplier * t_multiplier;
+            else {
+                tracks.push({
+                    id: t,
+                    weight: multiplier * t_multiplier
+                });
+            }
+        });
+    });
+
+    let new_tracks = [];
+    async.each(tracks, (t, next) => {
+        Track.findById(t.id, (error, track) => {
+            if (error) next(error);
+            else {
+                new_tracks.push({
+                    id: t.id,
+                    name: track.name,
+                    weight: t.weight
+                });
+                next();
+            }
+        });
+    }, error => {
+        if (error) {
+            winston.error(error.stack);
+            res.status(500).json(errors[500]);
+        } else {
+            new_tracks = new_tracks.sort((a, b) => b.weight - a.weight);        
+            res.status(200).json({ tracks: new_tracks });
+        }
+    });
 }
