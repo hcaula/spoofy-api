3af0a898e46536b6abd83f04ba6a48b5714ded4f
hcaula <henrique.caula@hotmail.com>
2018-06-17 04:57:51 -0300

    Adds playlist based on share genre seeds route

--- app/v2/controllers/playlists.js
+++ app/v2/controllers/playlists.js
@@ -14,6 +14,7 @@ module.exports = function (app) {
     app.get('/api/v2/playlists/shared/tracks', auth, getUsers, sharedTracks);
 
     app.get('/api/v2/playlists/artists', auth, getUsers, artistsPlaylist);
+    app.get('/api/v2/playlists/seeds', auth, getUsers, seedsPlaylist);
 }
 
 const getUsers = function (req, res, next) {
@@ -127,9 +128,55 @@ const artistsPlaylist = function (req, res) {
                     res.status(500).json(errors[500]);
                 } else {
                     tracks.sort((a, b) => b.weight - a.weight);
-                    res.status(200).json({tracks: tracks});
+                    res.status(200).json({ tracks: tracks });
                 }
             });
         }
     });
+}
+
+const seedsPlaylist = function (req, res) {
+    const users = req.users;
+    const multipliers = (req.query.multipliers ? req.query.multipliers.split(',') : [].fill.call({ length: users.length }, 1));
+    const limit = 25;
+    const access_token = req.user.token.access_token;
+    const avaiable_seeds = require('../../../config/jsons/seeds');
+
+    let genres = getSharedGenres(users, multipliers);
+    genres = genres.filter(g => avaiable_seeds.includes(g.name));
+    genres = genres.slice(0, 5);
+
+    let genresStr = '';
+    genres.forEach(g => genresStr += g.name + ',');
+
+    const path = `/v1/recommendations/?limit=${limit}&seed_genres=${genresStr}`;
+
+    const options = {
+        host: 'api.spotify.com',
+        path: path,
+        method: 'GET',
+        headers: { 'Authorization': `Bearer ${access_token}` }
+    }
+
+    let tracks = [];
+    request('https', options, (error, response) => {
+        if (error) {
+            winston.error(error.stack);
+            res.status(500).json(errors[500]);
+        } else {
+            response.tracks.forEach(t => tracks.push({
+                name: t.name,
+                artist: t.artists[0].name,
+                album: t.album.name,
+                image: t.album.images[0].url,
+                href: t.href,
+                uri: t.uri,
+                id: t.id
+            }));
+
+            res.status(200).json({ genres: genres.map(g => g.name), tracks: tracks });
+        }
+    });
+
+
 }
